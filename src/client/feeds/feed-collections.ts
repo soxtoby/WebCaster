83ee40f739b284851935dc05c5c3cb07bf5147ab
Simon Oxtoby <simonoxtoby@gmail.com>
2026-02-22 07:57:43 +1000

    Fixing access from external URL

--- src/client/feeds/feed-collections.ts
+++ src/client/feeds/feed-collections.ts
@@ -4,6 +4,8 @@ import { QueryClient } from "@tanstack/react-query"
 import type { Feed } from "../../server/db/schema"
 import { api } from "../api"
 
+globalThis.crypto.randomUUID ??= createUuidV4; // Required by tanstack db, but only available in secure context
+
 export let queryClient = new QueryClient()
 
 export const feedCollection = createCollection(queryCollectionOptions<Feed>({
@@ -39,3 +41,13 @@ export const feedCollection = createCollection(queryCollectionOptions<Feed>({
         await api.feeds.delete.mutate({ id: feed.id })
     }
 }))
+
+function createUuidV4() {
+    let bytes = new Uint8Array(16);
+    let byte6 = bytes[6] ?? 0
+    let byte8 = bytes[8] ?? 0
+    bytes[6] = (byte6 & 0x0f) | 0x40
+    bytes[8] = (byte8 & 0x3f) | 0x80
+    let hex = Array.from(bytes, (byte) => byte.toString(16).padStart(2, '0')).join('')
+    return `${hex.slice(0, 8)}-${hex.slice(8, 12)}-${hex.slice(12, 16)}-${hex.slice(16, 20)}-${hex.slice(20)}` as const
+}
